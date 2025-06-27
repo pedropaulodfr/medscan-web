55f73afaaa678079c2a6be00acb197d71fdee8a6
ppdfr84 <pedro.rocha@csjsistemas.com.br>
2025-06-27 17:09:16 -0300

    Ajuste para obter o tenantId

--- src/api/useApi.jsx
+++ src/api/useApi.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getSessionCookie } from '../helpers/cookies';
 
-const tenantId = window.location.hostname.split('.')[0];
+const tenantId = window.location.hostname.split('.')[0].replace("www.", "");
 
 const api = axios.create({
     baseURL: tenantId == "localhost" ? "http://localhost:5284" : "https://medscan-api-ujft.onrender.com"
